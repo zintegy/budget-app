49ca521a98bbedc19447a07449c7cef3d62fc5ca
Yitz Deng <yitzdeng@gmail.com>
2021-04-27 15:46:56 -0700

    changing default loading site

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,12 @@ app.use((err, req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 });
