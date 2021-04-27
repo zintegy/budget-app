ca289d5d03baa5bd316f830dc891e40159a7497d
Yitz Deng <yitzdeng@gmail.com>
2021-04-26 22:21:20 -0700

    Changing to mongodb online

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ const port = process.env.PORT || 5000;
 mongoose.set('useFindAndModify', false);
 
 //mongoose.connect(process.env.DB, {useNewUrlParser: true})
-mongoose.connect('mongodb://localhost:27017,localhost:27018,localhost:27019/mern-starter', {useNewUrlParser: true, replicaSet: 'rs0'}) 
+//mongoose.connect('mongodb://localhost:27017,localhost:27018,localhost:27019/mern-starter', {useNewUrlParser: true, replicaSet: 'rs0'})
+//  .then(() => console.log("DB connected"))
+//  .catch(err => console.log(err));
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+mongoose.connect('mongodb+srv://' + username + ':' + password + '@cluster0.3xxik.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
