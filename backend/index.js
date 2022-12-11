9a64e3e94d78f5a5cb35fbf0a2983eb6ce4cbb30
Yitz Deng <yitzdeng@gmail.com>
2022-12-10 21:37:06 -0800

    specify delete in allow-methods

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,8 @@ mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE");
   next();
 });
 
