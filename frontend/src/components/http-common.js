b242f0e2edafe34ae9c8cf64160b02bb208d757c
Yitz Deng <yitzdeng@gmail.com>
2023-06-06 09:45:30 -0700

    Adding CORS

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://yitz-budget.deta.dev/",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "https://ydeng-budget.cyclic.app/"
   }
 });
