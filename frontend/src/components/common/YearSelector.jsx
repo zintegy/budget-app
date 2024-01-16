31472cc6ee7378c7f8ddb993bfce3d58c02f3f8c
Yitz Deng <yitzdeng@gmail.com>
2024-01-16 12:51:48 -0800

    Adding more years into the future

--- frontend/src/components/common/YearSelector.jsx
+++ frontend/src/components/common/YearSelector.jsx
@@ -15,6 +15,12 @@ const YearSelector = ({name, id, value, onChange}) => {
       <MenuItem value={2021}>2021</MenuItem>
       <MenuItem value={2022}>2022</MenuItem>
       <MenuItem value={2023}>2023</MenuItem>
+      <MenuItem value={2024}>2024</MenuItem>
+      <MenuItem value={2025}>2025</MenuItem>
+      <MenuItem value={2026}>2026</MenuItem>
+      <MenuItem value={2027}>2027</MenuItem>
+      <MenuItem value={2028}>2028</MenuItem>
+      <MenuItem value={2029}>2029</MenuItem>
     </Select>
 }
 
