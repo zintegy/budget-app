a21c4acc60f2181b441a1c3a63e7351f819b5bca
Yitz Deng <yitzdeng@gmail.com>
2024-11-01 20:59:36 -0700

    bypass the ngrok interstitial message

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -4,5 +4,6 @@ export default axios.create({
   baseURL: "https://malamute-definite-turtle.ngrok-free.app/",
   headers: {
     "Content-type": "application/json",
+    "ngrok-skip-browser-warning": "123", // ngrok interstitial bypass
   }
 });
