753af92f3e9e17d3e1766b7b5723320192c78390
Yitz Deng <yitzdeng@gmail.com>
2024-11-03 14:04:17 -0800

    use raspberry pi as webserver

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://malamute-definite-turtle.ngrok-free.app/",
+  baseURL: "https://trivially-renewing-hedgehog.ngrok-free.app/",
   headers: {
     "Content-type": "application/json",
     "ngrok-skip-browser-warning": "123", // ngrok interstitial bypass
